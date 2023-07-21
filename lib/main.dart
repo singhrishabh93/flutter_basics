ef2fe3ec04fb1d572fd9479f82053d0f43f44b58
singhrishabh93 <singhrishabh1670@gmal.com>
2023-07-21 11:14:44 +0530

    Diff b/w Button and InkWell

--- lib/main.dart
+++ lib/main.dart
@@ -25,80 +25,11 @@ class DashboardScreen extends StatelessWidget {
           title: Text("Dashboard"),
         ),
         body: Container(
-          color: Colors.amberAccent,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            // mainAxisAlignment: MainAxisAlignment.spaceAround,
-            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            // mainAxisAlignment: MainAxisAlignment.center,
-            // mainAxisAlignment: MainAxisAlignment.start,
-            // crossAxisAlignment: CrossAxisAlignment.center,
-            // crossAxisAlignment: CrossAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            // crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Text(
-                "A",
-                style: TextStyle(fontSize: 25),
-              ),
-              Text(
-                "B",
-                style: TextStyle(fontSize: 25),
-              ),
-              Column(
-                    children: [
-                      ElevatedButton(onPressed: (){
-                        print("Button Pressed");
-                      }, child: Text("Click")),
-                      ElevatedButton(onPressed: (){
-                        print("Button Pressed");
-                      }, child: Text("Click"))
-                    ],
-                  ),
-              Text(
-                "C",
-                style: TextStyle(fontSize: 25),
-              ),
-              Text(
-                "D",
-                style: TextStyle(fontSize: 25),
-              ),
-              Text(
-                "E",
-                style: TextStyle(fontSize: 25),
-              ),
-                ],
-              ),
-              Text(
-                "A",
-                style: TextStyle(fontSize: 25),
-              ),
-              Text(
-                "B",
-                style: TextStyle(fontSize: 25),
-              ),
-              Text(
-                "C",
-                style: TextStyle(fontSize: 25),
-              ),
-              Text(
-                "D",
-                style: TextStyle(fontSize: 25),
-              ),
-              Text(
-                "E",
-                style: TextStyle(fontSize: 25),
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    print("Button Pressed");
-                  },
-                  child: Text("Click Me"))
-            ],
-          ),
+          child: ElevatedButton(
+              onPressed: () {
+                print("Button Pressed");
+              },
+              child: Text("Click Me")),
         ));
   }
 }
