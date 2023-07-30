019f5edc2abe01de1173a9c6d3ea3e38b068f7b4
singhrishabh93 <singhrishabh1670@gmal.com>
2023-07-30 15:53:49 +0530

    Date and Time Picker

--- lib/main.dart
+++ lib/main.dart
@@ -32,22 +32,39 @@ class _MyHomePageState extends State<MyHomePage> {
           backgroundColor: Colors.green,
         ),
         body: Center(
-          child: Container(
-            width: 200,
-            height: 200,
-            child: Column(
-              children: [
-                Text(
-                  'Current Time ${DateFormat('jms').format(time)}',
-                  style: TextStyle(fontSize: 25),
-                ),
-                ElevatedButton(
-                    onPressed: () {
-                      setState(() {});
-                    },
-                    child: Text("Current Time"))
-              ],
-            ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Select Date",
+                style: TextStyle(fontSize: 25),
+              ),
+              ElevatedButton(
+                  onPressed: () async {
+                    DateTime? datePicked = await showDatePicker(
+                        context: context,
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime(2021),
+                        lastDate: DateTime(2026));
+
+                    if (datePicked != null) {
+                      print('Date Selected ${datePicked.day}');
+                    }
+                  },
+                  child: Text("Show")),
+              ElevatedButton(
+                  onPressed: () async {
+                    TimeOfDay? pickedTime = await showTimePicker(
+                        context: context,
+                        initialTime: TimeOfDay.now(),
+                        initialEntryMode: TimePickerEntryMode.input);
+
+                    if (pickedTime != null) {
+                      print('Time Picked ${pickedTime.hour}');
+                    }
+                  },
+                  child: Text("Show Time"))
+            ],
           ),
         ));
   }
