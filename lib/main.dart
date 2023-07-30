6e8dd2b366fa9be81e20e612313d8627ec5ea5fb
singhrishabh93 <singhrishabh1670@gmal.com>
2023-07-30 14:44:42 +0530

    Date and Time in Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-// 39
 import 'package:flutter/material.dart';
-import 'package:flutter_basics_1/ui_helper/util.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,19 +8,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "MyFirstApp",
       debugShowCheckedModeBanner: false,
+      title: 'MyFirstApp',
       theme: ThemeData(),
-      home: DashboardScreen(),
+      home: MyHomePage(),
     );
   }
 }
 
-class DashboardScreen extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     var time = DateTime.now();
-
     return Scaffold(
         appBar: AppBar(
           title: Text("Dashboard"),
@@ -35,9 +37,14 @@ class DashboardScreen extends StatelessWidget {
             child: Column(
               children: [
                 Text(
-                  "Current Time {$time.year}",
+                  'Current Time ${time.day}',
                   style: TextStyle(fontSize: 25),
                 ),
+                ElevatedButton(
+                    onPressed: () {
+                      setState(() {});
+                    },
+                    child: Text("Current Time"))
               ],
             ),
           ),
