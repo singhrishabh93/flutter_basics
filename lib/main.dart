6a1b66252aa61311a4fabfa55f7df017285683df
singhrishabh93 <singhrishabh1670@gmal.com>
2023-07-30 10:50:13 +0530

    Flutter Date and Time

--- lib/main.dart
+++ lib/main.dart
@@ -19,74 +19,28 @@ class MyApp extends StatelessWidget {
 }
 
 class DashboardScreen extends StatelessWidget {
-  var EmailText = TextEditingController();
-  var Password = TextEditingController();
   @override
   Widget build(BuildContext context) {
+    var time = DateTime.now();
+
     return Scaffold(
         appBar: AppBar(
           title: Text("Dashboard"),
           backgroundColor: Colors.green,
         ),
         body: Center(
-            child: Container(
-                width: 300,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    TextField(
-                      keyboardType: TextInputType.emailAddress,
-                      controller: EmailText,
-                      // enabled: false,
-                      decoration: InputDecoration(
-                          hintText: "Enter Email",
-                          focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(25),
-                              borderSide: BorderSide(
-                                color: Colors.amberAccent,
-                              )),
-                          enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(25),
-                              borderSide: BorderSide(
-                                color: Colors.black,
-                              )),
-                          disabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(25),
-                              borderSide: BorderSide(
-                                color: Colors.grey,
-                              )),
-                          //suffixText: "Username Exists",
-                          // suffixIcon: IconButton(
-                          //   icon: Icon(Icons.email),
-                          //   onPressed: () {
-                          //     print("Button pressed");
-                          //   },
-                          // ),
-                          prefixIcon: Icon(Icons.home)),
-                    ),
-                    Container(
-                      height: 30,
-                    ),
-                    TextField(
-                      keyboardType: TextInputType.number,
-                      controller: Password,
-                      obscureText: true,
-                      obscuringCharacter: '#',
-                      decoration: InputDecoration(
-                          prefixIcon: Icon(Icons.password),
-                          hintText: "Enter Password",
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(25),
-                              borderSide: BorderSide(color: Colors.blueGrey))),
-                    ),
-                    ElevatedButton(
-                        onPressed: () {
-                          String uEmail = EmailText.text.toString();
-                          String uPassword = Password.text;
-                          print("Email: $uEmail Password: $uPassword");
-                        },
-                        child: Text("Login"))
-                  ],
-                ))));
+          child: Container(
+            width: 200,
+            height: 200,
+            child: Column(
+              children: [
+                Text(
+                  "Current Time {$time.year}",
+                  style: TextStyle(fontSize: 25),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
