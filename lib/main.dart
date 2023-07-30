ba69637827dbf0746961da0484318d5e379bfdd0
singhrishabh93 <singhrishabh1670@gmal.com>
2023-07-30 15:00:18 +0530

    Flutter Date Format

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(MyApp());
@@ -37,7 +38,7 @@ class _MyHomePageState extends State<MyHomePage> {
             child: Column(
               children: [
                 Text(
-                  'Current Time ${time.day}',
+                  'Current Time ${DateFormat('jms').format(time)}',
                   style: TextStyle(fontSize: 25),
                 ),
                 ElevatedButton(
