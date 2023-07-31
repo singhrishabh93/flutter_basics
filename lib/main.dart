82a0eb286907390c32279df8cf3fc02321d9c0c8
singhrishabh93 <singhrishabh1670@gmal.com>
2023-07-31 17:07:52 +0530

    CallBack Function in Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -23,56 +23,17 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  callBack() {
+    print("Callback Function");
+  }
+
   @override
   Widget build(BuildContext context) {
-    var arrColors = [
-      Colors.red,
-      Colors.amberAccent,
-      Colors.black,
-      Colors.green,
-      Colors.deepPurple,
-      Colors.orange,
-      Colors.blue,
-      Colors.pinkAccent,
-    ];
     return Scaffold(
         appBar: AppBar(
           title: Text("Dashboard"),
           backgroundColor: Colors.green,
         ),
-        body: Padding( // Grid View cannot be used with Column Widget Doubt
-          padding: const EdgeInsets.all(8.0),
-          child: GridView.count(
-            crossAxisCount: 4,
-            crossAxisSpacing: 11,
-            mainAxisSpacing: 11,
-            children: [
-              Container(
-                color: arrColors[0],
-              ),
-              Container(
-                color: arrColors[1],
-              ),
-              Container(
-                color: arrColors[2],
-              ),
-              Container(
-                color: arrColors[3],
-              ),
-              Container(
-                color: arrColors[4],
-              ),
-              Container(
-                color: arrColors[5],
-              ),
-              Container(
-                color: arrColors[6],
-              ),
-              Container(
-                color: arrColors[7],
-              ),
-            ],       
-          ),
-        ));
+        body: ElevatedButton(onPressed: callBack, child: Text("Show")));
   }
 }
