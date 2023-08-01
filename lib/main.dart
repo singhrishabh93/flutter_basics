8eef8501c74e1cfba326d0cd5b8826573d676f3f
singhrishabh93 <singhrishabh1670@gmal.com>
2023-08-01 12:54:46 +0530

    Split the app into widget: Creating Custom Widget

--- lib/main.dart
+++ lib/main.dart
@@ -29,11 +29,108 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    var arrColors = [
+      Colors.red,
+      Colors.amberAccent,
+      Colors.black,
+      Colors.green,
+      Colors.deepPurple,
+      Colors.orange,
+      Colors.blue,
+      Colors.pinkAccent,
+    ];
     return Scaffold(
         appBar: AppBar(
           title: Text("Dashboard"),
           backgroundColor: Colors.green,
         ),
-        body: ElevatedButton(onPressed: callBack, child: Text("Show")));
+        body: Container(
+            child: Column(
+          children: [
+            Expanded(
+              flex: 2,
+              child: Container(
+                color: Colors.amberAccent,
+                child: ListView.builder(
+                  itemBuilder: ((context, index) => Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: SizedBox(
+                            width: 100,
+                            child: CircleAvatar(
+                              backgroundColor: Colors.white70,
+                            )),
+                      )),
+                  itemCount: 10,
+                  scrollDirection: Axis.horizontal,
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 5,
+              child: Container(
+                  color: Colors.orange,
+                  child: ListView.builder(
+                      itemBuilder: ((context, index) => Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: ListTile(
+                              leading: CircleAvatar(
+                                backgroundColor: Colors.white70,
+                              ),
+                              title: Text("Name"),
+                              subtitle: Text("Mobile No"),
+                              trailing: Icon(Icons.delete),
+                            ),
+                          )))),
+            ),
+
+            Expanded(
+                flex: 2,
+                child: Container(
+                  color: Colors.green,
+                  child: ListView.builder(
+                    itemBuilder: ((context, index) => Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            width: 200,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(11),
+                                color: Colors.blueGrey),
+                          ),
+                        )),
+                    itemCount: 10,
+                    scrollDirection: Axis.horizontal,
+                  ),
+                )),
+
+            Expanded(
+                flex: 1,
+                child: Container(
+                  color: Colors.purple,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: GridView.count(
+                      crossAxisCount: 4,
+                      crossAxisSpacing: 11,
+                      mainAxisSpacing: 11,
+                      children: [
+                        Container(
+                          color: arrColors[0],
+                        ),
+                        Container(
+                          color: arrColors[0],
+                        ),
+                        Container(
+                          color: arrColors[0],
+                        ),
+                        Container(
+                          color: arrColors[0],
+                        ),
+                      ],
+                    ),
+                  ),
+                )),
+            // Expanded(flex: 2, child: Container(color: Colors.pink)),
+          ],
+        )));
   }
 }
