a1dde1df304ae916a8597b2d923e3c9466cf1ad6
singhrishabh93 <singhrishabh1670@gmal.com>
2023-08-12 01:16:21 +0530

    Gradient Colours

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,4 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_basics_1/Intropage.dart';
-import 'package:flutter_basics_1/SplashPage.dart';
-import 'package:flutter_basics_1/Thirdpage.dart';
-import 'package:flutter_basics_1/detail_page.dart';
-import 'package:flutter_basics_1/widgets/rounded_btn.dart';
-import 'package:intl/intl.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(MyApp());
@@ -27,25 +18,126 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<StatefulWidget> createState() {
+    return MyHomeState();
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class MyHomeState extends State<MyHomePage> {
+  var wtController = TextEditingController();
+  var ftController = TextEditingController();
+  var inController = TextEditingController();
+  var result = "";
+  var bgColor = Colors.yellow.shade400;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Clipper"),
+          title: Text("Body Mass Index"),
           centerTitle: true,
           backgroundColor: Colors.purple,
         ),
-        body: Center(
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(71),
-            child: Image.asset(
-              "assets/images/wallpaper.jpg",
-              width: 500,
-              height: 300,
+        body: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+            Colors.orange.shade100,
+            Colors.green.shade100,
+            Colors.blue.shade100
+          ])),
+          child: Center(
+            child: Container(
+              width: 300,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(11.0),
+                    child: Text(
+                      "Enter the following details",
+                      style: TextStyle(fontSize: 21),
+                    ),
+                  ),
+                  TextField(
+                    controller: wtController,
+                    decoration: InputDecoration(
+                        label: Text("Enter your Weight in KGs"),
+                        prefixIcon: Icon(Icons.line_weight)),
+                    keyboardType: TextInputType.number,
+                  ),
+                  SizedBox(
+                    height: 21,
+                  ),
+                  TextField(
+                    controller: ftController,
+                    decoration: InputDecoration(
+                        label: Text("Enter your Height in feets"),
+                        prefixIcon: Icon(Icons.height)),
+                    keyboardType: TextInputType.number,
+                  ),
+                  SizedBox(
+                    height: 21,
+                  ),
+                  TextField(
+                    controller: inController,
+                    decoration: InputDecoration(
+                        label: Text("Enter your Height in Inches"),
+                        prefixIcon: Icon(Icons.height)),
+                    keyboardType: TextInputType.number,
+                  ),
+                  SizedBox(
+                    height: 21,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        var weight = wtController.text.toString();
+                        var feet = ftController.text.toString();
+                        var inch = inController.text.toString();
+
+                        if (weight != "" && feet != "" && inch != "") {
+                          //BMI
+
+                          var iWeight = int.parse(weight);
+                          var ifeet = int.parse(feet);
+                          var iInch = int.parse(inch);
+
+                          var tInch = (ifeet * 12) + iInch;
+                          var tCm = tInch * 2.54;
+                          var tM = tCm / 100;
+
+                          var bmi = iWeight / (tM * tM);
+                          var msg = "";
+
+                          if (bmi > 25) {
+                            msg = "You'are OverWeight!";
+                            bgColor = Colors.orange.shade300;
+                          } else if (bmi < 18) {
+                            msg = "You'are UnderWeight!";
+                            bgColor = Colors.red.shade300;
+                          } else {
+                            msg = "You'are Healthy!";
+                            bgColor = Colors.green;
+                          }
+
+                          result =
+                              "$msg \n Your BMI is: ${bmi.toStringAsFixed(2)}";
+
+                          setState(() {});
+                        } else {
+                          setState(() {
+                            result = "Please fill all the required blanks!";
+                          });
+                        }
+                      },
+                      child: Text("Calculate")),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    result,
+                    style: TextStyle(fontSize: 20),
+                  )
+                ],
+              ),
             ),
           ),
         ));
