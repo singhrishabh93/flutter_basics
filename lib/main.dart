dd14eac8619839a76dde5264b9db147b6c7bc23c
singhrishabh93 <singhrishabh1670@gmal.com>
2023-08-11 20:34:50 +0530

    ClipRRect Widget

--- lib/main.dart
+++ lib/main.dart
@@ -31,35 +31,22 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  var arrIndex = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("3D List"),
+          title: Text("Clipper"),
           centerTitle: true,
           backgroundColor: Colors.purple,
         ),
-        body: Center( //ListWheel ScrollView Widget
-          child: ListWheelScrollView(
-            itemExtent: 200,
-            children: arrIndex
-                .map((value) => Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        child: Center(
-                            child: Text(
-                          "$value",
-                          style: TextStyle(fontSize: 25),
-                        )),
-                        decoration: BoxDecoration(
-                            color: Colors.green,
-                            borderRadius: BorderRadius.circular(21)),
-                        width: double.infinity,
-                      ),
-                    ))
-                .toList(),
-            //height of a single widget
+        body: Center(
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(71),
+            child: Image.asset(
+              "assets/images/wallpaper.jpg",
+              width: 500,
+              height: 300,
+            ),
           ),
         ));
   }
