5ceee568ef9fad58dffc969678db836ed4345901
singhrishabh93 <singhrishabh1670@gmal.com>
2023-08-12 11:26:37 +0530

    Mapping List to Widget Method-2

--- lib/main.dart
+++ lib/main.dart
@@ -25,75 +25,16 @@ class MyHomePage extends StatefulWidget {
 
 class MyHomeState extends State<MyHomePage> {
   var arrData = [
-    // {
-    //   'name' : 'Aarav Patel',
-    //   'mobno' : '9999999999',
-    //   'unread' : '2'
-    // },
-    // {
-    //   'name' : 'Aarav Patel',
-    //   'mobno' : '9999999999',
-    //   'unread' : '2'
-    // },
-    // {
-    //   'name' : 'Aarav Patel',
-    //   'mobno' : '9999999999',
-    //   'unread' : '2'
-    // },
-    // {
-    //   'name' : 'Aarav Patel',
-    //   'mobno' : '9999999999',
-    //   'unread' : '2'
-    // },
-    // {
-    //   'name' : 'Aarav Patel',
-    //   'mobno' : '9999999999',
-    //   'unread' : '2'
-    // },
-    // {
-    //   'name' : 'Aarav Patel',
-    //   'mobno' : '9999999999',
-    //   'unread' : '2'
-    // },
-    // {
-    //   'name' : 'Aarav Patel',
-    //   'mobno' : '9999999999',
-    //   'unread' : '2'
-    // },
-    // {
-    //   'name' : 'Aarav Patel',
-    //   'mobno' : '9999999999',
-    //   'unread' : '2'
-    // },
-    // {
-    //   'name' : 'Aarav Patel',
-    //   'mobno' : '9999999999',
-    //   'unread' : '2'
-    // },
-    // {
-    //   'name' : 'Aarav Patel',
-    //   'mobno' : '9999999999',
-    //   'unread' : '2'
-    // },
-    "Riya Sharma",
-    "Arjun Kumar",
-    "Aisha Gupta",
-    "Rohan Singh",
-    "Zara Khan",
-    "Virat Mehta",
-    "Aanya Joshi",
-    "Rajan Patel",
-    "Anaya Kapoor",
-    "Aditya Shah",
-    "Kavya Desai",
-    "Arnav Choudhury",
-    "Esha Yadav",
-    "Advait Iyer",
-    "Diya Sharma",
-    "Naveen Verma",
-    "Aashi Reddy",
-    "Kabir Agrawal",
-    "Tanvi Malhotra",
+    {'name': 'Aarav Patel', 'mobno': '9999999999', 'unread': '2'},
+    {'name': 'Aman Singh', 'mobno': '8888888888', 'unread': '3'},
+    {'name': 'Abhinav Sharma', 'mobno': '7777777777', 'unread': '2'},
+    {'name': 'Rahul Singh', 'mobno': '6666666666', 'unread': '5'},
+    {'name': 'Elon Musk', 'mobno': '5555555555', 'unread': '8'},
+    {'name': 'Satya Nadela', 'mobno': '4444444444', 'unread': '1'},
+    {'name': 'Sunadar Pichai', 'mobno': '3333333333', 'unread': '10'},
+    {'name': 'Goldy Brar', 'mobno': '2222222222', 'unread': '21'},
+    {'name': 'Elvish Yadav', 'mobno': '1111111111', 'unread': '2'},
+    {'name': 'Gaurav Taneja', 'mobno': '0000000000', 'unread': '5'},
   ];
 
   @override
@@ -106,21 +47,17 @@ class MyHomeState extends State<MyHomePage> {
         ),
         body: Container(
           child: ListView(
-              children: arrData.map((value) {
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(11)),
-                  color: Colors.blue.shade100,
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Center(child: Text(value)),
-                ),
-              ),
-            );
-          }).toList()),
+              children: arrData
+                  .map((value) => ListTile(
+                        leading: Icon(Icons.account_circle),
+                        title: Text(value['name'].toString()),
+                        subtitle: Text(value['mobno'].toString()),
+                        trailing: CircleAvatar(
+                            radius: 12,
+                            backgroundColor: Colors.green,
+                            child: Text(value['unread'].toString())),
+                      ))
+                  .toList()),
         ));
   }
 }
