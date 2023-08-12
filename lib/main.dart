48fd71256152e0f7b6a8b1acfe9bd51b4b459ab6
singhrishabh93 <singhrishabh1670@gmal.com>
2023-08-12 21:19:52 +0530

    Tween Animation

--- lib/main.dart
+++ lib/main.dart
@@ -23,41 +23,43 @@ class MyHomePage extends StatefulWidget {
   }
 }
 
-class MyHomeState extends State<MyHomePage> {
-  var arrData = [
-    {'name': 'Aarav Patel', 'mobno': '9999999999', 'unread': '2'},
-    {'name': 'Aman Singh', 'mobno': '8888888888', 'unread': '3'},
-    {'name': 'Abhinav Sharma', 'mobno': '7777777777', 'unread': '2'},
-    {'name': 'Rahul Singh', 'mobno': '6666666666', 'unread': '5'},
-    {'name': 'Elon Musk', 'mobno': '5555555555', 'unread': '8'},
-    {'name': 'Satya Nadela', 'mobno': '4444444444', 'unread': '1'},
-    {'name': 'Sunadar Pichai', 'mobno': '3333333333', 'unread': '10'},
-    {'name': 'Goldy Brar', 'mobno': '2222222222', 'unread': '21'},
-    {'name': 'Elvish Yadav', 'mobno': '1111111111', 'unread': '2'},
-    {'name': 'Gaurav Taneja', 'mobno': '0000000000', 'unread': '5'},
-  ];
+class MyHomeState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late Animation animation;
+  late Animation colorAnimation;
+  late AnimationController animationController;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 4));
+    animation = Tween(begin: 0.0, end: 200.0).animate(animationController);
+    colorAnimation = ColorTween(begin: Colors.blue, end: Colors.green)
+        .animate(animationController);
+
+    animationController.addListener(() {
+      print(animation.value);
+      setState(() {});
+    });
+
+    animationController.forward();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Mapping Lists"),
+          title: Text("Tween Animation"),
           centerTitle: true,
           backgroundColor: Colors.purple,
         ),
-        body: Container(
-          child: ListView(
-              children: arrData
-                  .map((value) => ListTile(
-                        leading: Icon(Icons.account_circle),
-                        title: Text(value['name'].toString()),
-                        subtitle: Text(value['mobno'].toString()),
-                        trailing: CircleAvatar(
-                            radius: 12,
-                            backgroundColor: Colors.green,
-                            child: Text(value['unread'].toString())),
-                      ))
-                  .toList()),
+        body: Center(
+          child: Container(
+            width: animation.value,
+            height: animation.value,
+            color: colorAnimation.value,
+          ),
         ));
   }
 }
