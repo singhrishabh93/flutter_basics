d9c34421508534df38c9b3bb263a5992f98e3d80
Rishabh Singh <singhrishabh1670@gmail.com>
2023-08-15 23:54:38 +0530

    Shared Preferences Login

--- lib/SplashPage.dart
+++ lib/SplashPage.dart
@@ -1,37 +1,37 @@
-// import 'dart:async';
+import 'dart:async';
 
-// import 'package:flutter/material.dart';
-// import 'package:flutter_basics_1/Intropage.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_basics_1/Intropage.dart';
 
-// class SplashPage extends StatefulWidget {
-//   @override
-//   State<SplashPage> createState() => _SplashPageState();
-// }
+class SplashPage extends StatefulWidget {
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
 
-// class _SplashPageState extends State<SplashPage> {
-//   @override
-//   void initState() {
-//     super.initState();
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
 
-//     Timer(Duration(seconds: 5), () {
-//       Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
-//         return IntroPage();
-//       }));
-//     });
-//   }
+    Timer(Duration(seconds: 5), () {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
+        return IntroPage();
+      }));
+    });
+  }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Container(
-//         color: Colors.blue,
-//         child: Center(
-//             child: Text(
-//           "Classico",
-//           style: TextStyle(
-//               fontSize: 34, fontWeight: FontWeight.w700, color: Colors.white),
-//         )),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.blue,
+        child: Center(
+            child: Text(
+          "Classico",
+          style: TextStyle(
+              fontSize: 34, fontWeight: FontWeight.w700, color: Colors.white),
+        )),
+      ),
+    );
+  }
+}
