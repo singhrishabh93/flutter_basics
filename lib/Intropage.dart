52f81464913bb809cb86303c93323c2ab76f3396
Rishabh Singh <singhrishabh1670@gmail.com>
2023-08-15 23:43:31 +0530

    Shared Preferences Login

--- lib/Intropage.dart
+++ lib/Intropage.dart
@@ -5,31 +5,39 @@ class IntroPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Intro Page"),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Welcome To Goa",
-              style: TextStyle(fontSize: 25, fontWeight: FontWeight.w100),
-            ),
-            SizedBox(
-              height: 11,
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return MyHomePage();
-                  }));
-                },
-                child: Text("Next"))
-          ],
+        appBar: AppBar(
+          title: Text("Intro Page"),
+          centerTitle: true,
         ),
-      ),
-    );
+        body: Center(
+          child: Container(
+            width: 300,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.person,
+                  size: 65,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                      label: Text("Email"),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(21))),
+                ),
+                SizedBox(
+                  height: 11,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                      label: Text("Password"),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(21))),
+                ),
+                ElevatedButton(onPressed: () {}, child: Text("Login"))
+              ],
+            ),
+          ),
+        ));
   }
 }
