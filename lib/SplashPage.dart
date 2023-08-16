7a89ad4f13ce9b45bc8386d71881cc1287b9c98c
Rishabh Singh <singhrishabh1670@gmail.com>
2023-08-16 21:00:08 +0530

    Shared Preferences

--- lib/SplashPage.dart
+++ lib/SplashPage.dart
@@ -1,37 +1,52 @@
-import 'dart:async';
+// import 'dart:async';
 
-import 'package:flutter/material.dart';
-import 'package:flutter_basics_1/Intropage.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_basics_1/Intropage.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
 
-class SplashPage extends StatefulWidget {
-  @override
-  State<SplashPage> createState() => _SplashPageState();
-}
+// class SplashPage extends StatefulWidget {
+//   static const String KEYLOGIN = "login";
+//   @override
+//   State<SplashPage> createState() => _SplashPageState();
+// }
 
-class _SplashPageState extends State<SplashPage> {
-  @override
-  void initState() {
-    super.initState();
+// class _SplashPageState extends State<SplashPage> {
+//   @override
+//   void initState() {
+//     super.initState();
 
-    Timer(Duration(seconds: 5), () {
-      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
-        return IntroPage();
-      }));
-    });
-  }
+//     void whereToGo() async {
+//       var sharedPref = await SharedPreferences.getInstance();
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        color: Colors.blue,
-        child: Center(
-            child: Text(
-          "Classico",
-          style: TextStyle(
-              fontSize: 34, fontWeight: FontWeight.w700, color: Colors.white),
-        )),
-      ),
-    );
-  }
-}
+//       var isLoggedIn = sharedPref.getBool(SplashPage.KEYLOGIN);
+
+//       Timer(Duration(seconds: 5), () {
+
+//         if(isLoggedIn! = null){
+//           if(isLoggedIn){
+
+//           }
+//         }
+//         Navigator.pushReplacement(context,
+//             MaterialPageRoute(builder: (context) {
+//           return IntroPage();
+//         }));
+//       });
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Container(
+//         color: Colors.blue,
+//         child: Center(
+//             child: Text(
+//           "Classico",
+//           style: TextStyle(
+//               fontSize: 34, fontWeight: FontWeight.w700, color: Colors.white),
+//         )),
+//       ),
+//     );
+//   }
+// }
