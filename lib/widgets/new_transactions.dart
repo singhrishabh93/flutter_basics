03d8dcd36356bc0dd62734b69e5b25ffcf17b706
singhrishabh93 <singhrishabh1670@gmal.com>
2023-08-22 20:27:22 +0530

    Added User Input, List Scroll View

--- lib/widgets/new_transactions.dart
+++ lib/widgets/new_transactions.dart
@@ -1,49 +1,53 @@
 import 'package:flutter/material.dart';
 
-class NewTransaction extends StatelessWidget{
-
+class NewTransaction extends StatelessWidget {
+  final Function addTx;
   final titleController = TextEditingController();
   final amountController = TextEditingController();
 
-  NewTransaction({super.key});
-  
+  NewTransaction(this.addTx);
+
   @override
   Widget build(BuildContext context) {
     return Card(
-            elevation: 5,
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  TextField(
-                    controller: titleController,
-                    // onChanged: (value) {
-                    //   titleInput = value;
-                    // },
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(5)),
-                        labelText: "Title"),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  TextField(
-                    controller: amountController,
-                      // onChanged: (value) {
-                      //   amountInput = value;
-                      // },
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(5)),
-                          labelText: "Amount")),
-                  ElevatedButton(
-                      onPressed: () {}, child: const Text("Add Transaction"))
-                ],
-              ),
+      elevation: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            TextField(
+              controller: titleController,
+              // onChanged: (value) {
+              //   titleInput = value;
+              // },
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5)),
+                  labelText: "Title"),
             ),
-          );
+            const SizedBox(
+              height: 10,
+            ),
+            TextField(
+                keyboardType: TextInputType.number,
+                controller: amountController,
+                // onChanged: (value) {
+                //   amountInput = value;
+                // },
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5)),
+                    labelText: "Amount")),
+            ElevatedButton(
+                onPressed: () {
+                  addTx(titleController.text,
+                      double.parse(amountController.text));
+                },
+                child: const Text("Add Transaction"))
+          ],
+        ),
+      ),
+    );
   }
-
 }
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -2,75 +2,57 @@ import 'package:flutter/material.dart';
 import '../models/transaction.dart';
 import 'package:intl/intl.dart';
 
-class TransactionList extends StatefulWidget {
-  const TransactionList({super.key});
-
-  @override
-  _TransactionListState createState() => _TransactionListState();
-}
-
-class _TransactionListState extends State<TransactionList> {
-  final List<Transaction> _userTransaction = [
-    Transaction(
-      id: "t1",
-      title: "New Shoes",
-      amount: 99.99,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: "t2",
-      title: "New Socks",
-      amount: 29.99,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: "t3",
-      title: "Belt",
-      amount: 59.49,
-      date: DateTime.now(),
-    ),
-  ];
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
+  TransactionList(this.transactions);
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: _userTransaction.map((value) {
-        return Card(
-          child: Row(
-            children: [
-              Container(
-                margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.purple,
-                    width: 2,
-                  ),
-                ),
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  "\$${value.amount}",
-                  style: const TextStyle(
-                      fontWeight: FontWeight.w700,
-                      fontSize: 20,
-                      color: Colors.purple),
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+    return Container(
+      height: 500,
+      child: SingleChildScrollView(
+        child: Column(
+          children: transactions.map((value) {
+            return Card(
+              child: Row(
                 children: [
-                  Text(
-                    value.title,
-                    style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 15),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.purple,
+                        width: 2,
+                      ),
+                    ),
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "\$${value.amount}",
+                      style: const TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 20,
+                          color: Colors.purple),
+                    ),
                   ),
-                  Text(
-                    DateFormat.yMMMEd().format(value.date),
-                    style: const TextStyle(color: Colors.grey),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        value.title,
+                        style: const TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        DateFormat.yMMMEd().format(value.date),
+                        style: const TextStyle(color: Colors.grey),
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
-          ),
-        );
-      }).toList(),
+            );
+          }).toList(),
+        ),
+      ),
     );
   }
 }
