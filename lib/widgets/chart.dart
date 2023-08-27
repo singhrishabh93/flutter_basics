e82d50d0cbb385372daa9d50c77614deb51ca18e
Rishabh Singh <singhrishabh1670@gmail.com>
2023-08-27 10:56:43 +0530

    Loping through Lists (Adding Logic)

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_basics_1/models/transaction.dart';
+import 'package:intl/intl.dart';
 
-class Chart extends StatelessWidget {  
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+
+  Chart(this.recentTransactions);
+
+  List<Map<String, Object>> get groupedTransactionsValues {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
+      double totalSum = 0.0;
+
+      for (var i = 0; i < recentTransactions.length; i++) {
+        if (recentTransactions[i].date.day == weekDay.day &&
+            recentTransactions[i].date.month == weekDay.month &&
+            recentTransactions[i].date.year == weekDay.year) {
+          totalSum += recentTransactions[i].amount;
+        }
+      }
+
+      print(DateFormat.E(weekDay));
+      print(totalSum);
+
+      return {'DAY': DateFormat.E(weekDay), 'amount': totalSum};
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
-      // child: ,
+      child: Row(
+        children: [],
+      ),
     );
   }
 }
