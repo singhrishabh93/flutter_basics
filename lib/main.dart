2e18877b185ea5abf83f688d40228b50706afcfc
singhrishabh93 <singhrishabh1670@gmal.com>
2023-10-12 19:26:56 +0530

    Divider: Used to Divide the List items

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,70 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter_basics_1/Intropage.dart';
+// import 'package:flutter_basics_1/SplashPage.dart';
+// import 'package:flutter_basics_1/Thirdpage.dart';
+// import 'package:flutter_basics_1/widgets/rounded_btn.dart';
+// import 'package:intl/intl.dart';
+// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+// import 'dart:async';
+// import 'package:shared_preferences/shared_preferences.dart';
 
-/// Flutter code sample for [AlertDialog].
+void main() {
+  runApp(MyApp());
+}
 
-void main() => runApp(const AlertDialogExampleApp());
-
-class AlertDialogExampleApp extends StatelessWidget {
-  const AlertDialogExampleApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Alert')),
-        body: const Center(
-          child: DialogExample(),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      title: 'MyFirstApp',
+      theme: ThemeData(),
+      home: SplashPage(),
     );
   }
 }
 
-class DialogExample extends StatelessWidget {
-  const DialogExample({super.key});
+class SplashPage extends StatefulWidget {
+  @override
+  State<SplashPage> createState() => SplashPageState();
+}
+
+class SplashPageState extends State<SplashPage> {
+  static const String KEYLOGIN = "login";
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return TextButton(
-      onPressed: () => showDialog<String>(
-        context: context,
-        builder: (BuildContext context) => AlertDialog(
-          title: const Text('AlertDialog Title'),
-          content: const Text('AlertDialog description'),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () => Navigator.pop(context, 'Cancel'),
-              child: const Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: () => Navigator.pop(context, 'OK'),
-              child: const Text('OK'),
-            ),
-          ],
-        ),
+    return Scaffold(
+        body: Container(
+      height: 500,
+      width: 500,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            height: 30,
+            width: double.infinity,
+            color: Colors.deepOrange,
+          ),
+          Divider(height: 10, color: Colors.black,),
+          Container(
+            height: 30,
+            width: double.infinity,
+            color: Colors.green,
+          ),
+          Divider(height: 10, color: Colors.black,),
+          Container(
+            height: 30,
+            width: double.infinity,
+            color: Colors.blue,
+          ),
+        ],
       ),
-      child: const Text('Show Dialog'),
-    );
+    ));
   }
 }
